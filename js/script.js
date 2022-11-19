a3c0139d4f129f702d00d7fc19f47814c4a20cb2
mwenda <brianmwenda255@gmail.com>
2022-11-19 18:39:59 +0300

    add functionality

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// typing animation
+var typed = new Typed (".typing",{
+    strings:["","Web Developer", "Web Designer","Mobile Developer", "Graphic Designer" ],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+// Aside
+const nav = document.querySelector(".nav"),
+  navList =nav.querySelectorAll("li"),
+  totalNavList = navList.length
+  for(let i=0; i<totalNavList; i++)
+  {
+   const  a = navList[i].querySelector("a");
+   a.addEventListener("click", function()
+   {
+    for(let j=0; j<totalNavList; j++)
+    {
+        navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active")
+
+   })
+  }
