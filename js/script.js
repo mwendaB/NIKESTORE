68faa9e38e0403853a96c0b7077653f9b65729f3
mwenda <brianmwenda255@gmail.com>
2022-11-22 10:32:13 +0300

    create  the toggle effect

--- js/script.js
+++ js/script.js
@@ -17,17 +17,39 @@ const nav = document.querySelector(".nav"),
    const  a = navList[i].querySelector("a");
    a.addEventListener("click", function()
    {
+    for(let i=0; i<totalSection; i++)
+    {
+        allsection[i].classList.remove("back-section");
+    }
     for(let j=0; j<totalNavList; j++)
     {
+        if(navList[j].querySelector("a").classList.contains("active"))
+        {
+            allsection[j].classList.add("back-section");
+        }
         navList[j].querySelector("a").classList.remove("active");
     }
     this.classList.add("active")
     showSection(this);
-
    })
   }
   function showSection(element)
   {
+    for(let i=0; i<totalSection; i++)
+    {
+        allsection[i].classList.remove("active");
+    }
     const target =element.getAtribute("href").split("#")[1];
     document.querySelector("#" + target).classList.add("active")
   }
+  const navTogglerBtn =document.querySelector(".nav-toggler"),
+        aside =document.querySelector(".aside");
+        navTogglerBtn.addEventListener("click", () =>
+        {
+            asideSectionTogglerBtn();
+        })
+        function asideSectionTogglerBtn()
+        {
+            aside.classList.toggle("open");
+            navTogglerBtn.classList.toggle("open");
+        }
